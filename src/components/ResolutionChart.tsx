53bbca759ac6a810dd249b539c137470fac85359
Grace Wang <gracewang92@gmail.com>
2018-12-29 23:44:57 -0800

    single component for percentile chart + table

--- src/components/ResolutionChart.tsx
+++ src/components/ResolutionChart.tsx
@@ -1,18 +1,24 @@
+import { HTMLSelect, HTMLTable } from '@blueprintjs/core';
 import * as React from "react";
 
 import ReactEcharts from "echarts-for-react";
 import percentile from 'percentile';
-import { IIssueData, IIssueDetails, IPRData, IPRDetails } from '../models/RepoData';
-import { toDays } from '../utils';
-import { PercentileLinks } from './PercentileLinks';
+import { ICIData, ICIDetails, IIssueData, IIssueDetails, IPRData, IPRDetails } from '../models/RepoData';
+import './ResolutionChart.css'
 
-type Data = IIssueData | IPRData
-type Details = IIssueDetails | IPRDetails
+type Data = ICIData | IIssueData | IPRData
+type Details = ICIDetails | IIssueDetails | IPRDetails
 
 interface IResolutionChartProps {
-    title: string
+    chartTitle: string
     items: Data[]
     loading: boolean
+    linkSelector: (d: Details) => React.ReactNode
+    linkLabel: string
+    valueSelector: (d: Details) => number
+    valueLabel: string
+    unitConverter: (n: number) => string
+    unitLabel: string
 }
 
 interface IWeekAndDetails {
@@ -20,49 +26,76 @@ interface IWeekAndDetails {
     details: Details
 }
 
-export class ResolutionChart extends React.Component<IResolutionChartProps> {
+interface IResolutionChartState {
+    selectedPercentile: string
+}
+
+export class ResolutionChart extends React.Component<IResolutionChartProps, IResolutionChartState> {
+
+    constructor(props: IResolutionChartProps) {
+        super(props);
+        this.state = { selectedPercentile: "p50" }
+    }
 
     public render() {
         const items = this.props.items.filter(({ details }) => details !== null);
         const p50 = items.map(({ week, details }) =>
-            ({ week, details: percentile(50, details!, (item: Details) => item.resolutionTime) }))
+            ({ week, details: percentile(50, details!, this.props.valueSelector) }))
         const p75 = items.map(({ week, details }) =>
-            ({ week, details: percentile(75, details!, (item: Details) => item.resolutionTime) }))
+            ({ week, details: percentile(75, details!, this.props.valueSelector) }))
         const p90 = items.map(({ week, details }) =>
-            ({ week, details: percentile(90, details!, (item: Details) => item.resolutionTime) }))
-        const data = { p50, p75, p90 }
+            ({ week, details: percentile(90, details!, this.props.valueSelector) }))
+        const data: {[p: string]: IWeekAndDetails[]} = { p50, p75, p90 }
         return (
             <div className="em-chart-group">
                 <ReactEcharts showLoading={this.props.loading} option={this.getOption(data)} />
-                <PercentileLinks
-                    data={data}
-                    initialPercentile="p50"
-                    titleLabel={this.props.title}
-                    valueLabel="Resolution Time"
-                    titleSelector={this.titleSelector}
-                    valueSelector={this.valueSelector}
-                    unitLabel="days"
-                    unitConverter={toDays}
-                />
+                <div className="em-chart-details">
+                    <div className="em-percentile-links">
+                        View details for:
+                        <HTMLSelect onChange={this.handleChange}>
+                            {Object.keys(data).map(p => (<option key={p} value={p}>{p}</option>))}
+                        </HTMLSelect>
+                        <HTMLTable condensed={true}>
+                            <thead>
+                                <tr>
+                                    <th>Week</th>
+                                    <th>{this.props.linkLabel}</th>
+                                    <th>{this.props.valueLabel} ({this.props.unitLabel})</th>
+                                </tr>
+                            </thead>
+                            <tbody className={this.props.loading ? "loading" : undefined}>
+                                {data[this.state.selectedPercentile].map(({ week, details }) => (
+                                    <tr key={week}>
+                                        <td>{this.stripYear(week)}</td>
+                                        <td>{this.props.linkSelector(details)}</td>
+                                        <td>{this.props.unitConverter(this.props.valueSelector(details))}</td>
+                                    </tr>
+                                ))}
+                            </tbody>
+                        </HTMLTable>
+                    </div>
+                </div>
             </div>
         );
     }
 
+    private stripYear = (date: string) => {
+        return date.match(/\d{4}-\d{2}-\d{2}/) ? date.substr(5) : date;
+    }
 
-    private titleSelector = (d: Details) => (<a href={d.url}>#{d.number}: ${d.title}</a>)
-    private valueSelector = (d: Details) => d.resolutionTime
+    private handleChange = (event: React.ChangeEvent<HTMLSelectElement>) => this.setState({ selectedPercentile: event.target.value })
+
 
     private getOption = (data: { [percentile: string]: IWeekAndDetails[] }) => {
         return {
             legend: {},
             series: Object.keys(data).map(p => ({
-                data: data[p].map(({ week, details }) => [week, toDays(details.resolutionTime)]),
+                data: data[p].map(({ week, details }) => [week, this.props.unitConverter(this.props.valueSelector(details))]),
                 name: p,
                 type: 'line',
-                yAxisIndex: 0
             })),
             title: {
-                text: `${this.props.title} Resolution Time`
+                text: this.props.chartTitle
             },
             tooltip: {
                 trigger: 'axis'
@@ -74,7 +107,7 @@ export class ResolutionChart extends React.Component<IResolutionChartProps> {
             ],
             yAxis: [
                 {
-                    name: `Resolution Time (days)`,
+                    name: `${this.props.valueLabel} (${this.props.unitLabel})`,
                     type: 'value'
                 },
             ],
